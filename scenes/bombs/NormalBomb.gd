8a8aa1cfdb80ed035f808d4a05f5c2c456ef0351
jokw1228 <jokw1228@naver.com>
2024-05-06 17:49:28 +0900

    플레이어 이동 선입력 매커니즘 구현

--- scenes/bombs/NormalBomb.gd
+++ scenes/bombs/NormalBomb.gd
@@ -6,3 +6,7 @@ func slayed():
 
 func exploded():
 	queue_free()
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		slayed()
--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,11 +1,18 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var PlayerRayCast2D: RayCast2D
 
 signal slay
 
+var moving_queue: Array
+var is_moving = false
+
 func _draw():
 	draw_arc(position, 32, 0, 2 * PI, 9, Color.WHITE, 4.0, false)
+
+func _physics_process(_delta):
+	_moving_queue_proccessing()
+	move_and_slide()
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
@@ -13,8 +20,20 @@ func _input(event):
 		slay.emit()
 
 func _on_player_ray_cast_2d_move_player(position_to_go):
-	var tween = create_tween()
-	var d = position_to_go - position
-	var distance: float = sqrt(d.x * d.x + d.y * d.y)
-	var speed: float = 2048
-	tween.tween_property(self, "position", position_to_go, distance / speed)
+	moving_queue.push_back(position_to_go)
+
+func _moving_queue_proccessing():
+	if is_moving == false && not moving_queue.is_empty():
+		is_moving = true
+		var position_to_go = moving_queue.pop_front()
+		
+		var speed: float = 2048
+		velocity = (position_to_go - position).normalized() * speed
+		var d = position_to_go - position
+		var distance: float = sqrt(d.x * d.x + d.y * d.y)
+		var timer = get_tree().create_timer(distance / speed)
+		await timer.timeout
+		velocity = Vector2.ZERO
+		position = position_to_go
+		
+		is_moving = false
