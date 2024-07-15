fcc805c9671f5eee47980908cac3a67356340716
jyeeon <174247274+jyeeon@users.noreply.github.com>
2024-07-15 19:19:11 +0900

    RoomMenu 구성요소 구현

---
+++ scenes/bombgenerators/MenuBombGenerator.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func select_circle():
+	print("circle")
+
+
+func select_circler():
+	print("circler")
+
+
+func select_circlest():
+	print("circlest")
+
+
+func select_hyper():
+	print("hyper")
---
+++ scenes/rooms/RoomMenu/PanelInfo.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var info0: String
+@export var info1: String
+@export var info2: String
+
+@export var label0: Label
+@export var shadow0: Label
+@export var label1: Label
+@export var shadow1: Label
+@export var label2: Label
+@export var shadow2: Label
+
+func _ready():
+	label0.text = info0
+	shadow0.text = info0
+	label1.text = info1
+	shadow1.text = info1
+	label2.text = info2
+	label2.text = info2
