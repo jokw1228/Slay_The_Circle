f10fa45973d06002317dc1f6eb40eaa6b5169453
ky-0922 <kky085437@korea.ac.kr>
2024-07-15 11:09:48 +0900

    RotationSpeedUpBomb 수정

--- scenes/bombs/RotationSpeedUpBomb.gd
+++ scenes/bombs/RotationSpeedUpBomb.gd
@@ -1,6 +1,8 @@
 extends Bomb
 class_name RotationSpeedUpBomb
 
+@export var rotation_speed_up_value: float = 0.1
+
 func slayed():
-	PlayingFieldInterface.rotation_speed_up(PI / 16)
+	PlayingFieldInterface.rotation_speed_up(rotation_speed_up_value)
 	super()
