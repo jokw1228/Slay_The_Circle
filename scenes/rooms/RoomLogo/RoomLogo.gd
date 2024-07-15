08e70a95461f05c2d1cef81966e23f14f9869e73
rammer7412 <coderammer7412@gmail.com>
2024-07-15 18:18:53 +0900

    2024.07.15. 18:18

--- scenes/rooms/RoomLogo/RoomLogo.gd
+++ scenes/rooms/RoomLogo/RoomLogo.gd
@@ -4,13 +4,28 @@ class_name RoomLogo
 @export var room_menu: PackedScene
 @export var Logos_node: Node2D
 @export var RoomMainMenu_room: PackedScene
+@export var LogoSound: AudioStream
+var sound_player: AudioStreamPlayer
 
 func _ready():
+	#playsound
+	sound_player=AudioStreamPlayer.new()
+	add_child(sound_player)
+	sound_player.stream=LogoSound
+	sound_player.play()
+	#로고 밝아지게 하기
 	var tween1=get_tree().create_tween()
-	var tween2=get_tree().create_tween()
-	tween1.tween_property(Logos_node, "modulate", Color(0,0,1,1), 2.0)
-	#await tween.finished
-	await get_tree().create_timer(2.0).timeout
+	tween1.tween_property(Logos_node, "modulate", Color(1,1,1,0), 0.0)
+	await tween1.finished
+	tween1 = get_tree().create_tween()
+	tween1.tween_property(Logos_node, "modulate", Color(1, 1, 1, 1), 5.0)
+	await get_tree().create_timer(5.0).timeout
+	await tween1.finished
+	
+	tween1 = get_tree().create_tween()
+	tween1.tween_property(Logos_node, "modulate", Color(1, 1, 1, 0), 0.0)
+	
+	#룸 메뉴로 이동
 	get_tree().change_scene_to_packed(room_menu)
 	
 
