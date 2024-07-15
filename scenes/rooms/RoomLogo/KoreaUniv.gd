e323bf8116e5a9cc781fb5f986990f0a185efd5b
rammer7412 <coderammer7412@gmail.com>
2024-07-15 12:07:07 +0900

    07.15.12:07

---
+++ scenes/rooms/RoomLogo/KoreaUniv.gd
@@ -0,0 +1,1 @@
+xtends Sprite2D
--- scenes/rooms/RoomLogo/RoomLogo.gd
+++ scenes/rooms/RoomLogo/RoomLogo.gd
@@ -2,11 +2,16 @@ extends Node2D
 class_name RoomLogo
 
 @export var room_menu: PackedScene
+@export var Logos_node: Node2D
+@export var RoomMainMenu_room: PackedScene
 
 func _ready():
-	'''
-	Write your code here!
-	'''
-	await get_tree().create_timer(1.0).timeout
+	var tween1=get_tree().create_tween()
+	var tween2=get_tree().create_tween()
+	tween1.tween_property(Logos_node, "modulate", Color(0,0,1,1), 2.0)
+	#await tween.finished
+	await get_tree().create_timer(2.0).timeout
 	get_tree().change_scene_to_packed(room_menu)
-	pass
+	
+
+	
