b820cbe70269b5ada29132a41710ab7356a7d00d
hihello122 <tprmatpxkr@gmail.com>
2024-07-16 17:17:45 +0900

    gameover 이슈 해결

--- scenes/bombs/Bomb.gd
+++ scenes/bombs/Bomb.gd
@@ -13,5 +13,6 @@ func slayed(): # safely defuse this bomb
 
 func exploded(): # game over
 	var current_position = self.position
-	PlayingFieldInterface.game_over(current_position)
+	PlayingFieldInterface.game_over()
+	PlayingFieldInterface.gameover_camera(current_position)
 	queue_free()
