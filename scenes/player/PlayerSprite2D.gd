20f347857cc08df16d1a36e628b6334220734a4b
jokw1228 <jokw1228@naver.com>
2024-07-20 17:24:59 +0900

    misc

--- scenes/player/PlayerSprite2D.gd
+++ scenes/player/PlayerSprite2D.gd
@@ -3,14 +3,27 @@ extends AnimatedSprite2D
 @export var Player_node: Player
 @export var PlayerRayCast2D: RayCast2D
 
+var ready_to_shoot: bool = false
+var ready_to_land: bool = false
+
 func _on_player_grounded():
-	animation = "laser_landing"
-	play()
+	stop()
+	play("laser_landing")
+	ready_to_land = true
 
 func _on_player_shooted():
+	stop()
 	rotation = PlayerRayCast2D.rotation
-	animation = "laser"
+	play_backwards("laser_landing")
+	ready_to_shoot = true
 
 func _on_animation_finished():
-	rotation = atan2(Player_node.position.y, Player_node.position.x)
-	animation = "landed"
+	if ready_to_shoot == true:
+		stop()
+		play("laser")
+		ready_to_shoot = false
+	elif ready_to_land == true:
+		stop()
+		rotation = atan2(Player_node.position.y, Player_node.position.x)
+		play("landed")
+		ready_to_land = false
