9cd556db671aa973e4a0872e226aa8678f4b1dda
jokw1228 <jokw1228@naver.com>
2024-07-20 17:34:23 +0900

    플레이어 스프라이트 위치 조정

--- scenes/player/PlayerSprite2D.gd
+++ scenes/player/PlayerSprite2D.gd
@@ -7,23 +7,25 @@ var ready_to_shoot: bool = false
 var ready_to_land: bool = false
 
 func _on_player_grounded():
-	stop()
+	offset.x = -48
 	play("laser_landing")
 	ready_to_land = true
+	ready_to_shoot = false
 
 func _on_player_shooted():
-	stop()
 	rotation = PlayerRayCast2D.rotation
+	offset.x = 0
 	play_backwards("laser_landing")
 	ready_to_shoot = true
+	ready_to_land = false
 
 func _on_animation_finished():
 	if ready_to_shoot == true:
-		stop()
+		offset.x = 0
 		play("laser")
 		ready_to_shoot = false
 	elif ready_to_land == true:
-		stop()
 		rotation = atan2(Player_node.position.y, Player_node.position.x)
+		offset.x = -48
 		play("landed")
 		ready_to_land = false
