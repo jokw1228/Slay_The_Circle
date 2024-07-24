da89b33251001f6cb3b4eaf6159268282d102451
jokw1228 <jokw1228@naver.com>
2024-07-24 22:47:01 +0900

    폭탄 타이머 스프라이트 간소화

--- scenes/bombgenerators/KangwooBombGenerator.gd
+++ scenes/bombgenerators/KangwooBombGenerator.gd
@@ -1,5 +1,6 @@
 extends BombGenerator
 
 func _ready():
-	create_hazard_bomb(Vector2(100, -100), 1.0, 0.5)
-	pass # Replace with function body.
+	while true:
+		create_gamespeedup_bomb(Vector2(0, 0), 1.0, 4, 10)
+		await get_tree().create_timer(5.0).timeout
