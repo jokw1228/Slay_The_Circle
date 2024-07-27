68130cd3b9f396e155c699aced3cc7c5f88be11f
jokw1228 <jokw1228@naver.com>
2024-07-27 22:26:13 +0900

    Flame 이펙트 약간 수정

--- scenes/bombs/StartBomb.gd
+++ scenes/bombs/StartBomb.gd
@@ -8,5 +8,5 @@ func slayed():
 	super()
 
 func _draw():
-	draw_circle(Vector2(0,0), 30, Color(1,1,1))
-	draw_circle(Vector2(0,0), 20, PlayingFieldInterface.get_theme_color())
+	draw_circle(Vector2(0,0), 30, PlayingFieldInterface.get_theme_color())
+	draw_circle(Vector2(0,0), 20, Color.BLUE)
