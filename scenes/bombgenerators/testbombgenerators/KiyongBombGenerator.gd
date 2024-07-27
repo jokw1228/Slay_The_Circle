511fa5163ed70f7f4f50f3a195398bd80e5c08b1
ky-0922 <kky085437@korea.ac.kr>
2024-07-28 06:34:26 +0900

    kiyong pattern 1,2

--- scenes/bombgenerators/testbombgenerators/KiyongBombGenerator.gd
+++ scenes/bombgenerators/testbombgenerators/KiyongBombGenerator.gd
@@ -1,43 +1,69 @@
 extends BombGenerator
 
-func _ready():
-	await get_tree().create_timer(3.0).timeout
-	
-	pattern4()
-	await get_tree().create_timer(5.0).timeout
-	pattern2()
-	await get_tree().create_timer(5.0).timeout
-	pattern3()
-	await get_tree().create_timer(5.0).timeout
-	pattern4()
+@export var timer = false
+@export var elapsed_time = 0.0
 
+@export var pattern2_bomb: Array[Node2D]
+@export var pattern2_moving = false
+
+func _ready():
+	timer = true
+	await get_tree().create_timer(1.0).timeout
+	
+	pattern2()
+	await get_tree().create_timer(12.0).timeout
+	
+	pattern2()
+
+
+func _process(delta):
+	if timer:
+		elapsed_time += delta
+	pattern2_process(delta)
 
 
 func pattern1():
-	create_hazard_bomb(Vector2(-80, 80), 1, 3)
-	create_hazard_bomb(Vector2(80, -80), 1, 3)
-	create_hazard_bomb(Vector2(-160, 160), 1, 3)
-	create_hazard_bomb(Vector2(160, -160), 1, 3)
-	create_normal_bomb(Vector2(0, 0), 1, 3)
+	pattern1_outer(elapsed_time)
+	for i in range(0,5):
+		var rotation_value = randf_range(0,2*PI)
+		var dist = randf_range(0,120)
+		create_normal_bomb(Vector2(dist*cos(rotation_value), dist*sin(rotation_value)), 0.5, 2)
+		await get_tree().create_timer(2).timeout
+		
+
+
+func pattern1_outer(time: float):
+	var start_time = time
+	var rotation_value = 0
+	while elapsed_time < start_time + 10:
+		create_hazard_bomb(Vector2(220*sin(rotation_value), 220*cos(rotation_value)), 0.5, 0.2)
+		rotation_value += PI / 8
+		await get_tree().create_timer(0.07).timeout
 
 
 func pattern2():
-	create_normal_bomb(Vector2(0, 100), 1, 3)
-	create_hazard_bomb(Vector2(100, 100), 1, 3)
-	create_hazard_bomb(Vector2(-100, 100), 1, 3)
-	create_hazard_bomb(Vector2(0, 0), 1, 3)
+	create_hazard_bomb(Vector2(180, 60), 1, 3)
+	create_hazard_bomb(Vector2(-180, 60), 1, 3)
+	create_hazard_bomb(Vector2(180, -60), 1, 3)
+	create_hazard_bomb(Vector2(-180, -60), 1, 3)
+	var bomb1 = create_normal_bomb(Vector2(0, 0), 1, 3)
+	var bomb2 = create_normal_bomb(Vector2(0, 0), 1, 3)
+	Utils.attach_node(pattern2_bomb[0], bomb1)
+	Utils.attach_node(pattern2_bomb[1], bomb2)
+	create_bomb_link(bomb1, bomb2)
+
+const pattern2_speed = 500
+var bomb_direction = [1, -1]
+var bomb_dir_changed = [false, false]
 
 
-func pattern3():
-	create_hazard_bomb(Vector2(-100, -100), 1, 3)
-	create_hazard_bomb(Vector2(-130, -80), 1, 3)
-	create_hazard_bomb(Vector2(-80, -130), 1, 3)
-	create_normal_bomb(Vector2(-160, -160), 1, 3)
+func pattern2_process(delta):
+	pattern2_bomb[0].position.x += pattern2_speed * bomb_direction[0] * delta
+	pattern2_bomb[1].position.x += pattern2_speed * bomb_direction[1] * delta
+	for i in range(2):
+		if (pattern2_bomb[i].position.x > 170 or pattern2_bomb[i].position.x < -170) and bomb_dir_changed[i] == false:
+			bomb_dir_changed[i] = true
+			bomb_direction[i] *= -1
+		else:
+			bomb_dir_changed[i] = false
 
-
-func pattern4():
-	create_numeric_bomb(Vector2(0, 250), 1, 3, 1)
-	create_numeric_bomb(Vector2(-60, -220), 1, 3, 2)
-	create_numeric_bomb(Vector2(220, 60), 1, 3, 3)
-	create_numeric_bomb(Vector2(-220, 60), 1, 3, 4)
-	create_numeric_bomb(Vector2(60, -220), 1, 3, 5)
