b033f21e20e1557f969f719b2a5b01a66ee4bd5d
jokw1228 <jokw1228@naver.com>
2024-07-27 18:47:50 +0900

    스테이지 룸 눈뽕 정상화
    신창섭

--- scenes/bombs/StartBomb.gd
+++ scenes/bombs/StartBomb.gd
@@ -6,3 +6,7 @@ signal started
 func slayed():
 	started.emit()
 	super()
+
+func _draw():
+	draw_circle(Vector2(0,0), 30, Color(1,1,1))
+	draw_circle(Vector2(0,0), 20, PlayingFieldInterface.get_theme_color())
