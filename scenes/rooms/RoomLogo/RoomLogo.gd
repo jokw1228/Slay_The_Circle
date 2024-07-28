89bd6f628d2a27cae478569f6a6da98f6b2d0160
jokw1228 <jokw1228@naver.com>
2024-07-28 16:16:54 +0900

    RoomLogo 정상화

--- scenes/rooms/RoomLogo/RoomLogo.gd
+++ scenes/rooms/RoomLogo/RoomLogo.gd
@@ -1,44 +1,54 @@
 extends Node2D
-class_name RoomLogo
-signal room_transition_fadein
-signal room_transition_fadeout
-signal room_color_change
 
-@export var room_menu: PackedScene
-@export var Logos_node: Node2D
-#@export var LogoSound: AudioStream
-#var sound_player: AudioStreamPlayer
-var room_transition_scene: PackedScene = load("res://scenes/rooms/RoomTransition/RoomTransition.tscn")
+var RoomMenu_room = "res://scenes/rooms/RoomMenu/RoomMenu.tscn"
 
+@onready var KoreaUniv_node: Sprite2D = $CanvasLayer/KoreaUniv
+@onready var Catdog_node: Sprite2D = $CanvasLayer/Catdog
+@onready var BackGroundBlack_node: ColorRect = $CanvasLayer/BackGroundBlack
+@onready var CircleField_node: CircleField = $CircleField
+@onready var Camera2D_node: Camera2D = $Camera2D
 
 func _ready():
-	# playsound
-	#sound_player=AudioStreamPlayer.new()
-	#add_child(sound_player)
-	#sound_player.stream=LogoSound
-	#sound_player.volume_db = -15
-	#sound_player.play()
-	SoundManager.loaded.connect(on_sound_manager_loaded)
-	#로고 밝아지게 하기
-	var tween1=get_tree().create_tween()
-	tween1.tween_property(Logos_node, "modulate", Color(1,1,1,0), 0.0)
-	await tween1.finished
-	emit_signal("room_transition_fadein")
-	emit_signal("room_color_change")
-	tween1 = get_tree().create_tween()
-	tween1.tween_property(Logos_node, "modulate", Color(1, 1, 1, 1), 4.0)
-	await get_tree().create_timer(4.0).timeout
-	await tween1.finished
+	CircleField_node.stop_reverb_effect()
 	
-	tween1 = get_tree().create_tween()
-	tween1.tween_property(Logos_node, "modulate", Color(1, 1, 1, 0), 0.0)
-	await tween1.finished
+	await get_tree().create_timer(0.5).timeout
 	
-	emit_signal("room_transition_fadeout")
+	var tween_korea_univ_on: Tween = get_tree().create_tween()
+	tween_korea_univ_on.set_ease(Tween.EASE_OUT)
+	tween_korea_univ_on.set_trans(Tween.TRANS_CIRC)
+	tween_korea_univ_on.tween_property(KoreaUniv_node, "position", Vector2(288, 324), 1)
+	
+	var tween_catdog_on: Tween = get_tree().create_tween()
+	tween_catdog_on.set_ease(Tween.EASE_OUT)
+	tween_catdog_on.set_trans(Tween.TRANS_CIRC)
+	tween_catdog_on.tween_property(Catdog_node, "position", Vector2(864, 324), 1)
+	
+	await tween_korea_univ_on.finished
+	await get_tree().create_timer(1).timeout
+	
+	var tween_korea_univ_off: Tween = get_tree().create_tween()
+	tween_korea_univ_off.set_ease(Tween.EASE_IN)
+	tween_korea_univ_off.set_trans(Tween.TRANS_CIRC)
+	tween_korea_univ_off.tween_property(KoreaUniv_node, "position", Vector2(864, 324), 1)
+
+	var tween_catdog_off: Tween = get_tree().create_tween()
+	tween_catdog_off.set_ease(Tween.EASE_IN)
+	tween_catdog_off.set_trans(Tween.TRANS_CIRC)
+	tween_catdog_off.tween_property(Catdog_node, "position", Vector2(288, 324), 1)
+	
+	await tween_korea_univ_off.finished
+	await get_tree().create_timer(0.5).timeout
+	
+	var tween_white: Tween = get_tree().create_tween()
+	tween_white.set_ease(Tween.EASE_IN_OUT)
+	tween_white.set_trans(Tween.TRANS_SINE)
+	tween_white.tween_property(BackGroundBlack_node, "modulate", Color.TRANSPARENT, 1)
+
+	var tween_camera: Tween = get_tree().create_tween()
+	tween_camera.set_ease(Tween.EASE_IN_OUT)
+	tween_camera.set_trans(Tween.TRANS_SINE)
+	tween_camera.tween_property(Camera2D_node, "zoom", Vector2(1, 1), 1.5)
+	
+	await tween_camera.finished
 	await get_tree().create_timer(1.0).timeout
-
-	get_tree().change_scene_to_packed(room_menu)
-
-func on_sound_manager_loaded() -> void:
-	SoundManager.play("sfx_logo","fade_in")
-	
+	get_tree().change_scene_to_file(RoomMenu_room)
