7c56655c99e3469eb93f21d96a1720c5392d2a4f
jokw1228 <jokw1228@naver.com>
2024-07-29 21:48:11 +0900

    NormalBomb 이미지 변경

--- scenes/bombs/NormalBomb.gd
+++ scenes/bombs/NormalBomb.gd
@@ -5,7 +5,5 @@ func slayed():
 	super()
 	
 	SoundManager.play("sfx_Nor_bomb","slay")
-	
-func _draw():
-	draw_circle(Vector2(0,0), 30, PlayingFieldInterface.get_theme_color())
-	draw_circle(Vector2(0,0), 20, Color.WHITE)
+
+
