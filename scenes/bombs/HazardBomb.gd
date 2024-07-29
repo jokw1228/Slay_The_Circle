b8ea7f2e013b06e0ade0cc3b2bbda5aec4ec84af
jokw1228 <jokw1228@naver.com>
2024-07-29 21:23:33 +0900

    BombTimer, HazardBomb 이미지 변경

--- scenes/bombs/HazardBomb.gd
+++ scenes/bombs/HazardBomb.gd
@@ -1,7 +1,2 @@
 extends Bomb
 class_name HazardBomb
-@export var font : Font
-func _draw():
-	draw_circle(Vector2(0,0), 30, Color(0,0,0))
-	draw_circle(Vector2(0,0), 20, Color(1,1,0))
-	draw_string(font, Vector2(-4,12), "!", HORIZONTAL_ALIGNMENT_LEFT, -5, 30, PlayingFieldInterface.color)
