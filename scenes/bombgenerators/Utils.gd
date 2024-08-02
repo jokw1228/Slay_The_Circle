5c145d5fab55153533a8d42961bd30e085ff4ec3
jyeeon <174247274+jyeeon@users.noreply.github.com>
2024-08-02 12:57:25 +0900

    ui: UI 정상화, RoomMenu에 적용

--- scenes/bombgenerators/Utils.gd
+++ scenes/bombgenerators/Utils.gd
@@ -10,6 +10,31 @@ func attach_node(target: Node2D, node: Node2D):
 	fixed_node.append(node)
 	fixed_target.append(target)
 
+# alias
+func attach(target: Node2D, node: Node2D):
+	attach_node(target, node)
+
+# get_tree().create_timer().timeout 너무 길어..
+func timer(time: float):
+	return get_tree().create_timer(time).timeout
+
+# UI 애니메이션 구현할 때 사용
+# offset이 날아가는 거리, direction은 날아갈 방향
+var original_position: Dictionary
+var previous_tween: Dictionary
+func slide(node: Control, offset: float, direction: Vector2, duration: float):
+	if not original_position.has(node):
+		original_position[node] = node.position
+	if previous_tween.has(node):
+		previous_tween[node].kill()
+		previous_tween.erase(node)
+	node.position = original_position[node] - offset * direction
+	previous_tween[node] = tween()
+	previous_tween[node].tween_property(node, "position", node.position + offset * direction, duration)
+
+func tween():
+	return get_tree().create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+
 func _process(_delta):
 	for i in range(len(fixed_node)):
 		if not is_instance_valid(fixed_node[i]) or not is_instance_valid(fixed_target[i]):
---
+++ scenes/ui/FancyButton.gd
@@ -0,0 +1,28 @@
+extends Control
+
+signal pressed
+
+@export var start_button_effect: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	%Timer1.start()
+	await get_tree().create_timer(0.3).timeout
+	%Timer2.start()
+
+
+func _on_button_pressed():
+	SoundManager.play("sfx_menu","start")
+	pressed.emit()
+
+# TODO 타이머로부터 시그널 받아서 Panel 복제하여 scale 키우는 이펙트 구현하기
+
+func _on_timer_timeout():
+	var new_effect: Panel = %Effect.duplicate()
+	add_child(new_effect)
+	new_effect.visible = true
+	Utils.tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD)\
+		.tween_property(new_effect, "modulate", Color(255, 255, 255, 0), 0.5)
+	var tween = Utils.tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(new_effect, "scale", Vector2(1.3, 1.3), 0.5)
+	tween.tween_callback(new_effect.queue_free)
