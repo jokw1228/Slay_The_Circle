7b0ee07e257a2bc63ea35326b82571e7c938d4b0
jokw1228 <jokw1228@naver.com>
2024-08-02 21:23:34 +0900

    플레이어 이동 선입력 개수 제한 구현

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -17,7 +17,10 @@ func _physics_process(_delta):
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		click_queue.push_back(get_global_mouse_position())
+		# store mouse input coordinates in queue
+		# store up to 4
+		if click_queue.size() < 4:
+			click_queue.push_back(get_global_mouse_position())
 
 func _on_player_ray_cast_2d_move_player(position_to_go):
 	movement_queue.push_back(position_to_go)
@@ -25,11 +28,13 @@ func _on_player_ray_cast_2d_move_player(position_to_go):
 
 func _movement_queue_proccessing():
 	if is_moving == false && not movement_queue.is_empty():
+		# start moving
+		shooted.emit()
 		is_moving = true
 		var position_to_go = movement_queue.pop_front()
-		shooted.emit()
-		current_position=position_to_go # 이동 중에는 가야 하는 위치 저장하기
+		current_position = position_to_go # 이동 중에는 가야 하는 위치 저장하기
 		
+		# actual movement
 		var speed: float = 2048
 		velocity = (position_to_go - position).normalized() * speed
 		var d = position_to_go - position
@@ -38,18 +43,22 @@ func _movement_queue_proccessing():
 		await timer.timeout
 		velocity = Vector2.ZERO
 		position = position_to_go
-		#ground offset
+		
+		# adjust ground offset
 		const GROUND_OFFSET = 16
 		const CIRCLE_FIELD_RADIUS = 256
 		position = position.normalized() * (CIRCLE_FIELD_RADIUS - GROUND_OFFSET)
 		
+		# grounded signal emit
 		grounded.emit()
 		is_moving = false
+		
 	if is_moving == false && not click_queue.is_empty() && is_raycasting == false:
 		change_click_queue_to_movement_queue.emit(click_queue.pop_front())
 		is_raycasting = true
 
-func now_position()->Vector2:
-	if is_moving==false and click_queue.is_empty(): # 플레이어가 가만히 있는 경우
+func now_position() -> Vector2:
+	if is_moving == false and click_queue.is_empty(): # 플레이어가 가만히 있는 경우
 		return get_global_transform().origin
-	else: return current_position # 플레이어가 움직이는 중인 경우
+	else:
+		return current_position # 플레이어가 움직이는 중인 경우
