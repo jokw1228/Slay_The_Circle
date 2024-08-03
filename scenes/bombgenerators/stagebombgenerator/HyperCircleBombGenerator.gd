0de930f11c172ab3fbc588ab90b12ed411e2c277
jokw1228 <jokw1228@naver.com>
2024-08-03 22:34:47 +0900

    각 스테이지별 BombGenerator setup

---
+++ scenes/bombgenerators/stagebombgenerator/HyperCircleBombGenerator.gd
@@ -0,0 +1,30 @@
+extends BombGenerator
+class_name HyperCircleBombGenerator
+
+var pattern_list: Array[Callable]
+
+func _ready():
+	pattern_list_initialization()
+	
+	await Utils.timer(1.0) # game start time offset
+	
+	pattern_shuffle_and_draw()
+
+func pattern_list_initialization():
+	pattern_list.append(Callable(self, "pattern_test_1"))
+
+func pattern_shuffle_and_draw():
+	randomize()
+	var random_index: int = randi() % pattern_list.size()
+	pattern_list[random_index].call()
+	
+
+###############################
+# pattern_test_1 block start
+
+func pattern_test_1():
+	await Utils.timer(0.1) # do nothing
+	pattern_shuffle_and_draw()
+
+# pattern_test_1 block end
+###############################
