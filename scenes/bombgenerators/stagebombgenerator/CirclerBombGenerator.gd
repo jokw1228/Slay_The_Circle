5972752bb8f7bb1cb0e1dbf83fdcf1ec8f8e2886
jaeyongpark121 <jaeyongpark01210121@gmail.com>
2024-08-10 15:12:13 +0900

    pattern_wall_timing 수정

--- scenes/bombgenerators/stagebombgenerator/CirclerBombGenerator.gd
+++ scenes/bombgenerators/stagebombgenerator/CirclerBombGenerator.gd
@@ -55,43 +55,59 @@ func pattern_lightning():
 ###############################
 # pattern_wall_timing start
 # made by Jaeyong
+# get_tree().call_group("group_hazard_bomb", "early_eliminate")가 경고를 안제거해서 곤란합니다...ㅠㅠ
+
+const pattern_wall_timing_playing_time = 3
+var pattern_wall_timing_bomb_count: int
+var pattern_wall_timing_pos_nor: Vector2
+var pattern_wall_timing_angle: float
+const pattern_wall_timing_bomb_dist = 136
 
 func pattern_wall_timing():
-	var left_bomb = 4
-	
-	var player_pos_normalized = PlayingFieldInterface.get_player_position().normalized()
-	if (player_pos_normalized == Vector2(0,0)):
-		player_pos_normalized = Vector2(1,0)
-	
-	create_normal_bomb(player_pos_normalized * 256 * -0.6 + Vector2(randf_range(0,80),randf_range(0, 40)), 0.25, 2.75)
-	create_normal_bomb(player_pos_normalized * 256 * -0.6 + Vector2(randf_range(-80,0),randf_range(-40,0)), 0.25, 2.75)
-	
-	create_normal_bomb(player_pos_normalized * 256 * 0.6 + Vector2(randf_range(0,80),randf_range(0, 40)), 0.25, 2.75)
-	create_normal_bomb(player_pos_normalized * 256 * 0.6 + Vector2(randf_range(-80,0),randf_range(-40,0)), 0.25, 2.75)
-	
-	
-	create_hazard_bomb(Vector2(0,0), 0.5, 0.5)
-	for i in (6):
-		create_hazard_bomb(player_pos_normalized.rotated(PI/2) * 256 * i/6, 0.5, 0.5)
-		create_hazard_bomb(player_pos_normalized.rotated(PI/2) * 256 * -i/6, 0.5, 0.5)
+	pattern_wall_timing_bomb_count = 4
+	pattern_wall_timing_pos_nor = PlayingFieldInterface.get_player_position().normalized()
+	if (pattern_wall_timing_pos_nor == Vector2(0,0)):
+		pattern_wall_timing_pos_nor = Vector2(1,0)
+	pattern_wall_timing_angle = pattern_wall_timing_pos_nor.angle()
+
+	var bomb1 = create_normal_bomb(pattern_wall_timing_auto_rotate(PI /4), 0.25, 2.75)
+	bomb1.connect("player_body_entered", Callable(self, "pattern_wall_timing_end"))
+	var bomb2 = create_normal_bomb(pattern_wall_timing_auto_rotate(3 * PI / 2 + PI/4), 0.25, 2.75)
+	bomb2.connect("player_body_entered", Callable(self, "pattern_wall_timing_end"))
+	create_bomb_link(bomb1, bomb2)
 		
-	await get_tree().create_timer(1).timeout
+	var bomb3 = create_normal_bomb(pattern_wall_timing_auto_rotate(PI + PI/4), 0.25, 2.75)
+	bomb3.connect("player_body_entered", Callable(self, "pattern_wall_timing_end"))
+	var bomb4 = create_normal_bomb(pattern_wall_timing_auto_rotate(PI/2 + PI/4), 0.25, 2.75)
+	bomb4.connect("player_body_entered", Callable(self, "pattern_wall_timing_end"))
+	create_bomb_link(bomb3, bomb4)
 	
 	create_hazard_bomb(Vector2(0,0), 0.5, 0.5)
-	for i in (6):
-		create_hazard_bomb(player_pos_normalized.rotated(PI/2) * 256 * i/6, 0.5, 0.5)
-		create_hazard_bomb(player_pos_normalized.rotated(PI/2) * 256 * -i/6, 0.5, 0.5)
-	
-	await get_tree().create_timer(1).timeout
-	
-	create_hazard_bomb(Vector2(0,0), 0.5, 0.5)
-	for i in (6):
-		create_hazard_bomb(player_pos_normalized.rotated(PI/2) * 256 * i/6, 0.5, 0.5)
-		create_hazard_bomb(player_pos_normalized.rotated(PI/2) * 256 * -i/6, 0.5, 0.5)
+	for i in (5):
+		create_hazard_bomb(pattern_wall_timing_pos_nor.rotated(PI/2) * 256 * i/5, 0.5, 0.5)
+		create_hazard_bomb(pattern_wall_timing_pos_nor.rotated(PI/2) * 256 * -i/5, 0.5, 0.5)
+		
+	create_hazard_bomb(Vector2(0,0), 1.5, 0.5)
+	for i in (5):
+		create_hazard_bomb(pattern_wall_timing_pos_nor.rotated(PI/2) * 256 * i/5, 1.5, 0.5)
+		create_hazard_bomb(pattern_wall_timing_pos_nor.rotated(PI/2) * 256 * -i/5, 1.5, 0.5)
+		
+	create_hazard_bomb(Vector2(0,0), 2.5, 0.5)
+	for i in (5):
+		create_hazard_bomb(pattern_wall_timing_pos_nor.rotated(PI/2) * 256 * i/5, 2.5, 0.5)
+		create_hazard_bomb(pattern_wall_timing_pos_nor.rotated(PI/2) * 256 * -i/5, 2.5, 0.5)
 
-	await Utils.timer(1) # do nothing
-	pattern_shuffle_and_draw()
+func pattern_wall_timing_end():
+	pattern_wall_timing_bomb_count -= 1
+	if pattern_wall_timing_bomb_count == 0:
+		get_tree().call_group("group_hazard_bomb", "early_eliminate")
+		await PlayingFieldInterface.player_grounded
+		PlayingFieldInterface.set_playing_time(pattern_start_time + (pattern_wall_timing_playing_time) / Engine.time_scale)
+		pattern_shuffle_and_draw()
 
+func pattern_wall_timing_auto_rotate(angle):
+	return Vector2(pattern_wall_timing_bomb_dist*cos(pattern_wall_timing_angle+angle),pattern_wall_timing_bomb_dist*sin(pattern_wall_timing_angle+angle))
+	
 # pattern_wall_timing block end
 ###############################
 
