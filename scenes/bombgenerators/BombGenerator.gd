73deeb87bf5dac53e210a4d7454a70da88443532
jokw1228 <jokw1228@naver.com>
2024-08-12 16:16:43 +0900

    call_deferred("add_child", bomb_inst)

--- scenes/bombgenerators/BombGenerator.gd
+++ scenes/bombgenerators/BombGenerator.gd
@@ -5,32 +5,32 @@ var bomb_link :PackedScene = preload("res://scenes/bombs/BombLink.tscn")
 
 func create_normal_bomb(bomb_position: Vector2, warning_time: float, bomb_time: float) -> NormalBomb:
 	var bomb_inst: NormalBomb = NormalBomb.create(bomb_position, warning_time, bomb_time)
-	add_child(bomb_inst)
+	call_deferred("add_child", bomb_inst)
 	return bomb_inst
 
 func create_hazard_bomb(bomb_position: Vector2, warning_time: float, bomb_time: float) -> HazardBomb:
 	var bomb_inst: HazardBomb = HazardBomb.create(bomb_position, warning_time, bomb_time)
-	add_child(bomb_inst)
+	call_deferred("add_child", bomb_inst)
 	return bomb_inst
 
 func create_numeric_bomb(bomb_position: Vector2, warning_time: float, bomb_time: float, bomb_id: int) -> NumericBomb:
 	var bomb_inst: NumericBomb = NumericBomb.create(bomb_position, warning_time, bomb_time, bomb_id)
-	add_child(bomb_inst)
+	call_deferred("add_child", bomb_inst)
 	return bomb_inst
 
 func create_rotationinversion_bomb(bomb_position: Vector2, warning_time: float, bomb_time: float) -> RotationInversionBomb:
 	var bomb_inst: RotationInversionBomb = RotationInversionBomb.create(bomb_position, warning_time, bomb_time)
-	add_child(bomb_inst)
+	call_deferred("add_child", bomb_inst)
 	return bomb_inst
 
 func create_rotationspeedup_bomb(bomb_position: Vector2, warning_time: float, bomb_time: float, speed_up_value: float) -> RotationSpeedUpBomb:
 	var bomb_inst: RotationSpeedUpBomb = RotationSpeedUpBomb.create(bomb_position, warning_time, bomb_time, speed_up_value)
-	add_child(bomb_inst)
+	call_deferred("add_child", bomb_inst)
 	return bomb_inst
 
 func create_gamespeedup_bomb(bomb_position: Vector2, warning_time: float, bomb_time: float, speed_up_value: float) -> GameSpeedUpBomb:
 	var bomb_inst: GameSpeedUpBomb = GameSpeedUpBomb.create(bomb_position, warning_time, bomb_time, speed_up_value)
-	add_child(bomb_inst)
+	call_deferred("add_child", bomb_inst)
 	return bomb_inst
 
 func create_bomb_link(bomb1: Bomb, bomb2: Bomb) -> BombLink:
