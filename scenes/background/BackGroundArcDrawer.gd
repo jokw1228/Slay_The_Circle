de7087021527060f0399b87b73eca86f15a25a1c
jokw1228 <jokw1228@naver.com>
2024-08-16 16:37:03 +0900

    misc

--- scenes/background/BackGroundArcDrawer.gd
+++ scenes/background/BackGroundArcDrawer.gd
@@ -22,7 +22,7 @@ func _draw():
 	var bright: float = (1.0 - base) * theme_bright * 0.9
 	var arc_color: Color = Color(theme_color.r * base + bright, theme_color.g * base + bright, theme_color.b * base + bright, 1)
 	
-	draw_arc(Vector2.ZERO, radius, 0, radian, 64, arc_color, arc_width, true)
+	draw_arc(Vector2.ZERO, radius, 0, radian, 24, arc_color, arc_width, true)
 	draw_circle(Vector2(radius, 0), arc_width / 2.0, arc_color)
 	draw_circle(Vector2(radius * cos(radian), radius * sin(radian)), arc_width / 2.0, arc_color) 
 
