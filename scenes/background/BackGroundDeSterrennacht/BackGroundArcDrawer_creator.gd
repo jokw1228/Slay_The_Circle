a852a16976ea6010c34ab7554cf6ec9f6643118a
jokw1228 <jokw1228@naver.com>
2024-08-17 21:26:46 +0900

    불필요한 tscn 파일 제거

--- scenes/background/BackGroundDeSterrennacht/BackGroundArcDrawer_creator.gd
+++ scenes/background/BackGroundDeSterrennacht/BackGroundArcDrawer_creator.gd
@@ -1,10 +1,8 @@
 extends Node
 class_name BackGroundArcDrawer_creator
 
-const BackGroundArcDrawer_scene = "res://scenes/background/BackGroundDeSterrennacht/BackGroundArcDrawer.tscn"
-
 static func create(radius_to_set: float, radian_to_set: float, to_rotate: float) -> BackGroundArcDrawer:
-	var inst: BackGroundArcDrawer = preload(BackGroundArcDrawer_scene).instantiate() as BackGroundArcDrawer
+	var inst: BackGroundArcDrawer = BackGroundArcDrawer.new()
 	inst.radius = radius_to_set
 	inst.radian = radian_to_set
 	inst.rotate(to_rotate)
