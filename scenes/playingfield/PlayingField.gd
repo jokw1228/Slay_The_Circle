4a8d19470291369b9715d2209d418d4c64490d37
jaeyongpark121 <jaeyongpark01210121@gmail.com>
2024-08-18 20:10:45 +0900

    stage마다 음악 다르게 변경... 아직 circler랑 circlest는 같음

--- scenes/playingfield/PlayingField.gd
+++ scenes/playingfield/PlayingField.gd
@@ -40,12 +40,34 @@ func start_PlayingField():
 		
 		connect("game_over", Callable(BombGenerator_node, "queue_free"))
 		
-	if (MusicManager.is_playing("bgm_PF","playing_bgm")):
-		MusicManager.disable_stem("dead",0.5)
-		MusicManager.enable_stem("on_game",0.5)
-	else:
-		MusicManager.stop(0.5)
-		MusicManager.play("bgm_PF","playing_bgm",0.5,1)
+	if(PlayingFieldInterface.get_stage_index() == 0 || PlayingFieldInterface.get_stage_index() == 3):
+		#circle 음악 재생
+		if (MusicManager.is_playing("bgm_PF","playing_bgm")):
+			MusicManager.disable_stem("dead",0.5)
+			MusicManager.enable_stem("on_game",0.5)
+		else:
+			MusicManager.stop(0.5)
+			MusicManager.play("bgm_PF","playing_bgm",0.5,1)
+			
+	elif(PlayingFieldInterface.get_stage_index() == 1 || PlayingFieldInterface.get_stage_index() == 4):
+		#circler 음악 재생
+		if (MusicManager.is_playing("bgm_PF","playing_bgm_2")):
+			MusicManager.disable_stem("dead",0.5)
+			MusicManager.enable_stem("on_game",0.5)
+		else:
+			MusicManager.stop(0.5)
+			MusicManager.play("bgm_PF","playing_bgm_2",0.5,1)
+			
+	elif(PlayingFieldInterface.get_stage_index() == 2 || PlayingFieldInterface.get_stage_index() == 5):
+		#circlest 음악 재생
+		if (MusicManager.is_playing("bgm_PF","playing_bgm_3")):
+			MusicManager.disable_stem("dead",0.5)
+			MusicManager.enable_stem("on_game",0.5)
+		else:
+			MusicManager.stop(0.5)
+			MusicManager.play("bgm_PF","playing_bgm_3",0.5,1)
+
+
 
 func stop_PlayingField(bomb_position: Vector2):
 	if playing == true:
