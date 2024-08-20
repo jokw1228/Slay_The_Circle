5acd42781c781c9744791a389a719f70a3dea7c1
jokw1228 <jokw1228@naver.com>
2024-08-20 14:40:58 +0900

    Hyper mode에서 warning이 더 잘 보이도록 수정

--- scenes/bombs/bombeffects/BombTimer/WarningTimer.gd
+++ scenes/bombs/bombeffects/BombTimer/WarningTimer.gd
@@ -10,3 +10,9 @@ func _ready():
 	tween_time.tween_property(self, "value", 0.0, set_time)
 	await tween_time.finished
 	warning_timeout.emit()
+
+func _process(_delta):
+	var parent: Bomb = get_parent() as Bomb
+	var bright: float = PlayingFieldInterface.get_theme_bright()
+	if parent != null and value != 0:
+		parent.modulate.a = (32.0 + 32.0 * bright) / 256.0
